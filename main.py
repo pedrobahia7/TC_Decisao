d63ecba46ea23cc9032b4057876b64fa1428d558
pedrobahia7 <79110112+pedrobahia7@users.noreply.github.com>
2023-05-07 22:09:27 -0300

    fe_initial_solution

--- main.py
+++ main.py
@@ -9,13 +9,43 @@ gvns = GVNS( NeighbourStructs().structs )
 
 
 
-def fc_initial_solution(resources):
-    x = pd.DataFrame(np.zeros(resources.shape))
-    min_idx = resources.idxmin()
-    min_idx = min_idx.values.reshape(1,resources.shape[1])
-    for col in resources.columns:
+def fc_initial_solution(df):
+    x = pd.DataFrame(np.zeros(df.shape))
+    min_idx = df.idxmin()
+    min_idx = min_idx.values.reshape(1,df.shape[1])
+    for col in df.columns:
         x.loc[min_idx[0][col],col] = 1
     return(x)
+
+
+
+def fe_initial_solution(df):
+    cost = df
+    machine_vec = np.zeros(5)
+    x = pd.DataFrame(np.zeros(cost.shape))
+    cost_copy = cost.copy()
+
+
+    while cost.empty == False:
+
+        min_value = cost_copy.min().min()    
+        min_col = cost_copy.min().idxmin()  #task
+        min_row = cost_copy.idxmin().loc[min_col] #machine
+
+        if machine_vec[min_row] == 0:
+            x.loc[min_row,min_col] = 1 
+            machine_vec[min_row] = 1
+            cost.drop(min_col,axis = 1, inplace=True)
+            cost_copy.drop(min_col,axis = 1, inplace=True)
+            if machine_vec.sum() == 5:
+                machine_vec = np.zeros(5)
+                cost_copy = cost.copy()
+
+        if machine_vec[min_row] == 1:
+            cost_copy.loc[min_row,min_col] = 80000000
+
+    return(x)
+
 
 
 
