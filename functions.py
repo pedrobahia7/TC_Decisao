9077297a935fcbd557c7c3db7556ac3272d46d61
pedrobahia7 <79110112+pedrobahia7@users.noreply.github.com>
2023-05-07 23:56:48 -0300

    add f_c and f_e functions

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
-def f_C(): #TODO
-    pass
+def f_C(cost,x): #TODO
+    final_cost = cost.mul(x).sum().sum()
+    return(final_cost)
 
-def f_E(): #TODO
-    pass
+def f_E(resource,x): #TODO
+    CRA = resource.mul(x).sum(axis=1)
+    final_consumption = max(CRA) - min(CRA)
+    return(final_consumption)
+
