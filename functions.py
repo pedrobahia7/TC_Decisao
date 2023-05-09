227384893af4e375a2dff0ee94bae6185e49aa43
pedrobahia7 <79110112+pedrobahia7@users.noreply.github.com>
2023-05-09 01:41:53 -0300

    changes values returned by restriction functions

--- functions.py
+++ functions.py
@@ -2,13 +2,13 @@ from restrictions import capacidade_excedida
 
 def f_C(x, cost, max_capacity, resource): 
     if capacidade_excedida(x, resource, max_capacity):
-        return 50000
+        return 2000
     final_cost = cost.mul(x).sum().sum()
     return(final_cost)
 
 def f_E(x, cost, max_capacity, resource): 
     if capacidade_excedida(x, resource, max_capacity):
-        return 50000
+        return 200
     CRA = resource.mul(x).sum(axis=1)
     final_consumption = max(CRA) - min(CRA)
     return(final_consumption)
