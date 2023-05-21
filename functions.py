a436634f0e107187992739e77f24fb2f79a7fc05
saliba-san <gabriel.portela.saliba@gmail.com>
2023-05-21 20:30:26 -0300

    adiciona versao negativa das funcoes objetivo

--- functions.py
+++ functions.py
@@ -13,3 +13,16 @@ def f_E(x, cost, max_capacity, resource):
     final_consumption = max(CRA) - min(CRA)
     return(final_consumption)
 
+def neg_f_C(x, cost, max_capacity, resource): 
+    if capacidade_excedida(x, resource, max_capacity):
+        return 2000
+    final_cost = cost.mul(x).sum().sum()
+    return(-final_cost)
+
+def neg_f_E(x, cost, max_capacity, resource): 
+    if capacidade_excedida(x, resource, max_capacity):
+        return 200
+    CRA = resource.mul(x).sum(axis=1)
+    final_consumption = max(CRA) - min(CRA)
+    return(-final_consumption)
+
