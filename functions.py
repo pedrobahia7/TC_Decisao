ff657bba082d379e362441bc31ee4d6b8ca55fbd
Pedrobahia7 <pedrobahia7@hotmail.com>
2023-05-21 20:30:23 -0300

    move initial value functions from main.py to functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 from restrictions import capacidade_excedida
+import pandas as pd
+import numpy as np
 
 def f_C(x, cost, max_capacity, resource): 
     if capacidade_excedida(x, resource, max_capacity):
@@ -13,3 +15,38 @@ def f_E(x, cost, max_capacity, resource):
     final_consumption = max(CRA) - min(CRA)
     return(final_consumption)
 
+
+def fc_initial_solution(df):
+    x = pd.DataFrame(np.zeros(df.shape))
+    min_idx = df.idxmin()
+    min_idx = min_idx.values.reshape(1,df.shape[1])
+    for col in df.columns:
+        x.loc[min_idx[0][col],col] = 1
+    return(x)
+
+def fe_initial_solution(df):
+    cost = df.copy()
+    machine_vec = np.zeros(5)
+    x = pd.DataFrame(np.zeros(cost.shape))
+    cost_copy = cost.copy()
+    
+
+    while cost.empty == False:
+
+        min_value = cost_copy.min().min()    
+        min_col = cost_copy.min().idxmin()  #task
+        min_row = cost_copy.idxmin().loc[min_col] #machine
+
+        if machine_vec[min_row] == 0:
+            x.loc[min_row,min_col] = 1 
+            machine_vec[min_row] = 1
+            cost.drop(min_col,axis = 1, inplace=True)
+            cost_copy.drop(min_col,axis = 1, inplace=True)
+            if machine_vec.sum() == 5:
+                machine_vec = np.zeros(5)
+                cost_copy = cost.copy()
+
+        if machine_vec[min_row] == 1:
+            cost_copy.loc[min_row,min_col] = 80000000
+
+    return(x)
--- main.py
+++ main.py
@@ -1,44 +1,8 @@
 from neighbour_structs import NeighbourStructs
 from metaheuristics import GVNS
-from functions import f_C, f_E
+from functions import *
 import pandas as pd
 import numpy as np 
-
-def fc_initial_solution(df):
-    x = pd.DataFrame(np.zeros(df.shape))
-    min_idx = df.idxmin()
-    min_idx = min_idx.values.reshape(1,df.shape[1])
-    for col in df.columns:
-        x.loc[min_idx[0][col],col] = 1
-    return(x)
-
-def fe_initial_solution(df):
-    cost = df.copy()
-    machine_vec = np.zeros(5)
-    x = pd.DataFrame(np.zeros(cost.shape))
-    cost_copy = cost.copy()
-
-
-    while cost.empty == False:
-
-        min_value = cost_copy.min().min()    
-        min_col = cost_copy.min().idxmin()  #task
-        min_row = cost_copy.idxmin().loc[min_col] #machine
-
-        if machine_vec[min_row] == 0:
-            x.loc[min_row,min_col] = 1 
-            machine_vec[min_row] = 1
-            cost.drop(min_col,axis = 1, inplace=True)
-            cost_copy.drop(min_col,axis = 1, inplace=True)
-            if machine_vec.sum() == 5:
-                machine_vec = np.zeros(5)
-                cost_copy = cost.copy()
-
-        if machine_vec[min_row] == 1:
-            cost_copy.loc[min_row,min_col] = 80000000
-
-    return(x)
-
 
 n = 5
 m = 50
