f9b91dbbb3806a2355b24a7415991d2cf16cc3d1
Pedrobahia7 <pedrobahia7@hotmail.com>
2023-05-22 23:42:09 -0300

    corrects is_append condition and adds break to treat exception

--- epsilon_biobjective.py
+++ epsilon_biobjective.py
@@ -11,7 +11,8 @@ def check_frontier(solution,frontier):
     is_append = 1
     remove_list = list()
     for i in range(len(frontier)):
-
+        if solution == frontier[i]:
+            break
         aux = 0 
         if f_C(frontier[i], m_custo_tarefa, v_capacidade_maxima, m_recursos_necessarios) > aux_fc:
             aux = aux +1 
@@ -25,15 +26,15 @@ def check_frontier(solution,frontier):
         elif aux == 0:
             is_append = 0
 
-
+    
     for i in remove_list:
         frontier = frontier.remove(i)
         if frontier == None:
             frontier = list()
         
-    if is_append==0:
+    if is_append==1:
         frontier.append(solution)
-    
+        
 
 is_epsilon = 1
 
